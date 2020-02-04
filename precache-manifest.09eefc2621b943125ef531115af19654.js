7e39161817c1ce17d4ec8515a693512b2cdb3d09
Mr. Darkside <rachel@Mrs-MacBook-Pro.local>
2020-02-04 11:09:29 -0600

    Updates

---
+++ precache-manifest.09eefc2621b943125ef531115af19654.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ba9d0d99aa12b9207b853d8a4fdb7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc733df74740551398b4",
+    "url": "/static/css/main.773b9639.chunk.css"
+  },
+  {
+    "revision": "070363e3f685dab3097f",
+    "url": "/static/js/2.3852dc6b.chunk.js"
+  },
+  {
+    "revision": "17b9fda17569e68f5d953340cecb04ab",
+    "url": "/static/js/2.3852dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc733df74740551398b4",
+    "url": "/static/js/main.5146f331.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "e4b8d4ee30b928d596b2912cef7d469d",
+    "url": "/static/media/aboutPic.e4b8d4ee.jpg"
+  },
+  {
+    "revision": "be23303c32341373a20138f74150d023",
+    "url": "/static/media/bamazon.be23303c.PNG"
+  },
+  {
+    "revision": "20ff73260d908c029a78aa867bcce7e4",
+    "url": "/static/media/emerald.20ff7326.png"
+  },
+  {
+    "revision": "de43668b4b4adb027fb9adc2ff6c8879",
+    "url": "/static/media/heart-solid.de43668b.svg"
+  },
+  {
+    "revision": "1ff87108529e0926d8ced69359e61f42",
+    "url": "/static/media/kanjiExamp.1ff87108.png"
+  },
+  {
+    "revision": "f7aa761347f506b27bc291957d319eb8",
+    "url": "/static/media/liri.f7aa7613.PNG"
+  },
+  {
+    "revision": "bbdb74cfd8bfa66faf199e3da2131e40",
+    "url": "/static/media/umnPic.bbdb74cf.png"
+  }
+]);
